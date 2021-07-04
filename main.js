401578ac229f91f30358b3236412ee0ee9e686b3
Yuvaan-Goel <78798914+Yuvaan-Goel@users.noreply.github.com>
2021-07-04 10:39:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    canvas.position(560, 375);
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/29uQc_ZRt/model.json', modelLoaded);
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+}
+
+
+function draw()
+{
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("result_object").innerHTML = results[0].label;
+        document.getElementById("result_accuracy").innerHTML = results[0].confidence.toFixed(3)*100 + "%";
+    }
+}
